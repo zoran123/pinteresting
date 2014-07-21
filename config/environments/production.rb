9bf9ef1a86b0012265b609409dec4ef796a51e5e
Zoran Martinovic <zoran.martinovic@gmail.com>
2014-07-20 23:17:00 -0700

    S3 image fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Pinteresting::Application.configure do
   # Sets up Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => ":s3_domain_url",
+    :path => ":class/:id.:style.:extension",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
